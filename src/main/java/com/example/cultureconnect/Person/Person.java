40b28453fe00c6fa29ac3149b648f60bb3731b52
andhot01 <andra.hotnogu@yahoo.com>
2024-04-24 12:13:53 +0200

    Klasser til lokation, person og projekt med tilsvarende konstrktører, getters og setters

---
+++ src/main/java/com/example/cultureconnect/Person/Person.java
@@ -0,0 +1,98 @@
+package com.example.cultureconnect.Person;
+
+import com.example.cultureconnect.Lokation.Lokation;
+import com.example.cultureconnect.Projekt.Projekt;
+import javafx.scene.image.Image;
+
+import java.util.List;
+
+public class Person {
+    private String name;
+    private List<Projekt> myProjects;
+    private String position;
+    private Lokation lokation;
+    private String arbejdsområde; //måske ikke nødvendig
+    private String email;
+    private int tlfNr;
+    private Image picture;
+
+    //måske booleans for creator / co-creator
+
+
+    public Person(String name, List<Projekt> myProjects, String position, Lokation lokation,
+                  String arbejdsområde, String email, int tlfNr, Image picture) {
+        this.name = name;
+        this.myProjects = myProjects;
+        this.position = position;
+        this.lokation = lokation;
+        this.arbejdsområde = arbejdsområde;
+        this.email = email;
+        this.tlfNr = tlfNr;
+        this.picture = picture;
+    } //konstruktør for person
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Projekt> getMyProjects() {
+        return myProjects;
+    }
+
+    public void setMyProjects(List<Projekt> myProjects) {
+        this.myProjects = myProjects;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Lokation getLokation() {
+        return lokation;
+    }
+
+    public void setLokation(Lokation lokation) {
+        this.lokation = lokation;
+    }
+
+    public String getArbejdsområde() {
+        return arbejdsområde;
+    }
+
+    public void setArbejdsområde(String arbejdsområde) {
+        this.arbejdsområde = arbejdsområde;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTlfNr() {
+        return tlfNr;
+    }
+
+    public void setTlfNr(int tlfNr) {
+        this.tlfNr = tlfNr;
+    }
+
+    public Image getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Image picture) {
+        this.picture = picture;
+    }
+    //getters og setters
+}
---
+++ src/main/java/com/example/cultureconnect/Projekt/Projekt.java
@@ -0,0 +1,77 @@
+package com.example.cultureconnect.Projekt;
+
+import com.example.cultureconnect.Person.Person;
+
+import java.util.Date;
+import java.util.List;
+
+public class Projekt {
+    private String titel;
+    private List<Person> participants;
+    private String description;
+    private Person projectCreator;
+    private Date startDate;
+    private Date endDate;
+
+    //ressourcer - eks. billeder og dokumenter skal muligvis tilføjes
+
+
+    public Projekt(String titel, List<Person> participants, String description,
+                   Person projectCreator, Date startDate, Date endDate) {
+        this.titel = titel;
+        this.participants = participants;
+        this.description = description;
+        this.projectCreator = projectCreator;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }//konstruktør for projekt
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public List<Person> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<Person> participants) {
+        this.participants = participants;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Person getProjectCreator() {
+        return projectCreator;
+    }
+
+    public void setProjectCreator(Person projectCreator) {
+        this.projectCreator = projectCreator;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    //getters og setters
+}
