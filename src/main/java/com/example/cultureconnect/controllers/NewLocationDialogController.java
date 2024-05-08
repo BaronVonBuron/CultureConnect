38da8c9abba512793886c842ee48900b2af64389
andhot01 <andra.hotnogu@yahoo.com>
2024-05-08 11:06:39 +0200

    choice box for ny lokation virker med ansvarlig mulighed, skal styles

--- src/main/java/com/example/cultureconnect/controllers/NewLocationDialogController.java
+++ src/main/java/com/example/cultureconnect/controllers/NewLocationDialogController.java
@@ -1,10 +1,16 @@
 package com.example.cultureconnect.controllers;
 
+import com.example.cultureconnect.Logic.Logic;
+import com.example.cultureconnect.Person.Person;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+
+import java.util.List;
 
 public class NewLocationDialogController {
 
@@ -12,7 +18,7 @@ public class NewLocationDialogController {
     private TextField ansvarligEmailNyLokationFelt;
 
     @FXML
-    private ChoiceBox<?> ansvarligVælger;
+    private ChoiceBox<String> ansvarligVælger;
 
     @FXML
     private TextField emailNyLokationFelt;
@@ -28,6 +34,23 @@ public class NewLocationDialogController {
 
     @FXML
     private TextField telefonnummerNyLokationFelt;
+
+    private Logic logic;
+
+    public void initialize(){
+        this.logic = Logic.getInstance();
+        choiceBoxOptions();
+    }
+
+    public void choiceBoxOptions(){
+        List<Person> persons = logic.getPersons();
+        ObservableList<String> ansvarligeMuligheder = FXCollections.observableArrayList();
+        for (Person person : persons) {
+            ansvarligeMuligheder.add(person.getName());
+        }
+        ansvarligVælger.setItems(ansvarligeMuligheder);
+        ansvarligVælger.setValue("Vælg ansvarlig");
+    }
 
     @FXML
     void opretNyLokationKnapPressed(ActionEvent event) {
